8fc483974ea1f4ec697edc97825b52367be94083
Ryan Lu <164600355+luryann@users.noreply.github.com>
2025-08-20 13:15:28 -0700

    Update basicInput.java

--- dailycoding/basicInput.java
+++ dailycoding/basicInput.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
-class Main
+public class Main
   {
     public static void main(String[] args)
     {
       Scanner scan = new Scanner(System.in);
-      String name1, name2, name3, name4; 
-      // Can stack these as such 
+      String example1, example2; 
       
-      System.out.println("Please enter four names:");
+      /*
+      You can stack then like this, or like
+      String example1;
+      String example2;
+      String example3;
+      */
       
-      // System.out.println("Enter the first name");
-      name1 = scan.nextLine();
-      // System.out.println("Enter the second name");
-      name2 = scan.nextLine();
-      // System.out.println("Enter the third name");
-      name3 = scan.nextLine();
-      // System.out.println("Enter the fourth name");
-      name4 = scan.nextLine();
-      
-      System.out.println(name4 + " " + name2 + " " + name3 + " " + name1);
+      System.out.println("Sample line");
+      example1 = scan.nextLine();
+      example2 = scan.nextLine();
+      System.out.println(example1 + " " + example2);
     }
   }
