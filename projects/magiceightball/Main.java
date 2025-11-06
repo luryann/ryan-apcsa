4a8dcbd30d98d0905c0b20c5cdc340688e0c9053
Ryan Lu <164600355+luryann@users.noreply.github.com>
2025-11-06 13:39:14 -0800

    Expand responses in getRandomAnswer method

--- projects/magiceightball/Main.java
+++ projects/magiceightball/Main.java
@@ -28,8 +28,28 @@ public class Main {
     }
 
     public static String getRandomAnswer() {
-        String responses = "It is certain,Most likely,Ask again later,Outlook not so good,Yes,No";
-        int randomNum = (int) (Math.random() * 6) + 1; // 1-6 inclusive
+        String responses = "It is certain,"
+                + "It is decidedly so,"
+                + "Without a doubt,"
+                + "Yes definitely,"
+                + "You may rely on it,"
+                + "As I see it, yes,"
+                + "Most likely,"
+                + "Outlook good,"
+                + "Yes,"
+                + "Signs point to yes,"
+                + "Reply hazy, try again,"
+                + "Ask again later,"
+                + "Better not tell you now,"
+                + "Cannot predict now,"
+                + "Concentrate and ask again,"
+                + "Don't count on it,"
+                + "My reply is no,"
+                + "My sources say no,"
+                + "Outlook not so good,"
+                + "Very doubtful";
+
+        int randomNum = (int) (Math.random() * 20) + 1; // 1-20 inclusive
 
         int start = 0;
         int end = responses.indexOf(',');
